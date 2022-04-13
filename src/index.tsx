9a5fb268100107da601f3f9bffe925c672bfdeb5
im-tejasvi <imtejasvi@gmail.com>
2022-04-13 20:40:55 +0530

    Implemented iframe and managed communication

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -20,10 +21,12 @@ const App = () => {
     startService();
   }, []);
 
-  const submitText = async () => {
+  const onClick = async () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -35,9 +38,30 @@ const App = () => {
         global: 'window'
       }
     });
-    console.log(result);
-    setCode(result.outputFiles[0].text);
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message',(event)=>{
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root=document.querySelector('#root');
+              root.innerHTML='<div style="color:red;"><h4>Runtime Error: </h4>' + err + '</div>';
+              console.error(err);
+            }
+          },false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -46,9 +70,14 @@ const App = () => {
         onChange={(event) => setInput(event.target.value)}
       ></textarea>
       <div>
-        <button onClick={submitText}>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
