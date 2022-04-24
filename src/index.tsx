ff40ab3b1abe0b03f4fecd0ac321ecc63fba3695
Tejasvi Sharma <imtejasvi@gmail.com>
2022-04-24 11:12:38 +0530

    Added text editor support in codebook

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
       {/* <CodeCell /> */}
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-// 6786
+// 6814
