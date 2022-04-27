04e61eb9e227896d68802970d4ebf32c803a7c04
Tejasvi Sharma <imtejasvi@gmail.com>
2022-04-27 11:07:12 +0530

    Started connecting react with redux with custom hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 import { store } from './state';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
-        {/* <CodeCell /> */}
+        <CellList />
       </div>
     </Provider>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-// 7827
+// 8282
