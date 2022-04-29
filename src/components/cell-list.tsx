0707a27649439ee2a2c34fd6bf5e69da592bca12
Tejasvi Sharma <imtejasvi@gmail.com>
2022-04-29 13:55:49 +0530

    Added functionality for action bar

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 
 const CellList = () => {
@@ -7,10 +9,18 @@ const CellList = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
